56882f12e984357c18728b9bd3d98934a32ccdce
topaaaan <topanyulianto01@gmail.com>
2020-05-22 22:43:04 +0700

    membuat tampilan slider

--- index.php
+++ index.php
@@ -8,16 +8,138 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="vendors/bootstrap-4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-4.4.1/css/bootstrap.min.css">
+
+    <!-- Slick CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick-1.8.1/slick/slick.css" />
+    <!-- // Add the new slick-theme.css if you want the default styling -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick-1.8.1/slick/slick-theme.css" />
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="assets/img/logo2.png" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active mr-3">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link" href="#">T&C</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link" href="#">Privacy Poilicy</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <button class="btn btn-outline-success my-2 my-sm-0 px-4 py-2 ml-3" type="submit">Market</button>
+                    <button class="btn btn-outline-success my-2 my-sm-0 px-4 py-2 ml-3" type="submit">Sign Up</button>
+                </form>
+            </div>
+    </nav>
+
+    <section id="banner">
+        <div class="container-fluid p-0">
+            <div class="banner-hero-home">
+                <div class="main-slider-banner-home">
+                    <div class="main-slider-home" style="background: url('assets/img/background/bg-homepage.png'); background-repeat:no-repeat; background-size:cover;">
+                        <div class="row d-flex justify-content-center align-items-center banner-home-content">
+                            <h1>Health From Food</h1>
+                        </div>
+                    </div>
+                    <div class="main-slider-home" style="background: url('assets/img/background/bg-homepage.png'); background-repeat:no-repeat; background-size:cover;">
+                        <div class="row d-flex justify-content-center align-items-center banner-home-content">
+                            <h1>Health From Food</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="thumbnail-slider-banner-home">
+                <div class="thumbnail-slider-home">
+                    <div class="img-fluid thumbnail-banner-home" style="background: url('assets/img/component/slider-home.png'); background-repeat:no-repeat; background-size:cover;">
+                    </div>
+                    <div class="img-fluid thumbnail-banner-home" style="background: url('assets/img/component/slider-home.png'); background-repeat:no-repeat; background-size:cover;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <section id="about">
+        <div class="container">
+            <div class="row text-center mt-5">
+                <div class="col">
+                    <h1>About Us</h1>
+                </div>
+            </div>
+
+            <div class="row mt-4 mb-5">
+                <div class="col-5">
+                    <img src="assets/img/component/about-home.png" class="img-fluid" alt="about-home">
+                </div>
+                <div class="col-7">
+                    <p>Pada section ini menjelaskan secara singkat mengenai perusahaan seperti visi dan misi singkat dalam sebuah paragraph dan juga pencapaian yang sudah diraih serta tujuan Utama perusahaan</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="founders">
+        <div class="container">
+            <div class="row text-center mt-5">
+                <div class="col">
+                    <h1></h1>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="vendors/jquery-3.4.1/jquery.min.js"></script>
+    <script src="assets/vendors/jquery-3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/vendors/slick-1.8.1/slick/slick.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="vendors/bootstrap-4.4.1/js/bootstrap.min.js"></script>
+    <script src="assets/vendors/bootstrap-4.4.1/js/bootstrap.min.js"></script>
+    <script>
+        $('.banner-hero-home').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.thumbnail-slider-banner-home'
+        });
+        $('.thumbnail-slider-banner-home').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.banner-hero-home',
+            dots: true,
+            centerMode: true,
+            focusOnSelect: true
+        });
+    </script>
 </body>
 
 </html>
